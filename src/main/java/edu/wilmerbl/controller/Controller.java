b4571773305d449e1cc50fecbde0c02dd79ad434
Wilmer Blossom <wilmerbl@pr-b1.kzn.21-school.ru>
2024-07-22 18:39:58 +0300

    add docker compose

--- src/main/java/edu/wilmerbl/controller/Controller.java
+++ src/main/java/edu/wilmerbl/controller/Controller.java
@@ -2,7 +2,6 @@ package edu.wilmerbl.controller;
 
 import edu.wilmerbl.models.File64;
 import edu.wilmerbl.repository.FileRepository;
-import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.ResourceAccessException;
 
 @RestController
 @RequestMapping(path = "/file")
