4ab316d2f2e1fc429e0ac1f24c532e06a2909cff
Wilmer Blossom <wilmerbl@pr-b1.kzn.21-school.ru>
2024-07-22 20:43:34 +0300

    add test

--- src/test/java/edu/wilmerbl/MainApplicationTests.java
+++ src/test/java/edu/wilmerbl/MainApplicationTests.java
@@ -1,13 +1,38 @@
 package edu.wilmerbl;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import edu.wilmerbl.models.File64;
+import edu.wilmerbl.repository.FileRepository;
+import java.util.Date;
+import java.util.UUID;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest
 class MainApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private FileRepository fileRepository;
+    String uuidString = "0bc1978c-a229-4ccc-adfc-3157910002df";
+    UUID uuid = UUID.fromString(uuidString);
+
+    @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+    @Test
+    void getProductById() throws Exception {
+        fileRepository.save(new File64(uuid, "text", new Date(), "text"));
+        assertNotNull(fileRepository.findById(uuid));
+    }
+
+//    @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+//    @Test
+//    void getProductById() throws Exception {
+//        fileRepository.save(new File64(uuid, "text", new Date(), "text"));
+//        assertNotNull(fileRepository.findById(uuid));
+//    }
+
 
 }
